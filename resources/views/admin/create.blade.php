96bf5d2a8d88a32a13e44a389df04af98401c587
Malika Zahro Nadia Aulia <malika.zna.27@gmail.com>
2025-05-26 13:06:46 +0700

    Update create.blade.php

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -32,7 +32,15 @@
             </div>
             <div class="isian">
                 <label>Foto</label>
-                <input type="file" name="foto">
+                <div class="up-file-container" style="display: flex; width: 800px;">
+                    <button class="up-file" type="button" onclick="document.getElementById('sampul').click();">
+                        <span class="material-symbols-outlined" style="vertical-align: middle;">upload</span>
+                        Pilih Foto
+                    </button>
+                    <input id="file-name" type="text" value="Belum memilih file" readonly>
+                    <input id="sampul" name="sampul" type="file" style="display: none;"
+                        onchange="document.getElementById('file-name').value = this.files[0]?.name || 'Belum memilih file';">
+                </div>
             </div>
             <div class="isian">
                 <label>Email</label>
